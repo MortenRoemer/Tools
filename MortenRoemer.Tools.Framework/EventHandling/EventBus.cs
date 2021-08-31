41056dbe5e58677dbb92f22bac4295c096453730
MuhammedAhyed <m.a.demirbas@live.de>
2021-08-31 17:03:10 +0200

    WIP: started reflection implementation

--- MortenRoemer.Tools.Framework/EventHandling/EventBus.cs
+++ MortenRoemer.Tools.Framework/EventHandling/EventBus.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 
 namespace MortenRoemer.Tools.Framework.EventHandling
 {
@@ -10,27 +13,27 @@ namespace MortenRoemer.Tools.Framework.EventHandling
         {
         }
 
-        public void AddHandler<THandler, TEvent>(ActiveEventHandler<THandler, TEvent> activeHandler)
-            where THandler : class, IEventHandler<TEvent>, new()
-        {
-            if (Handlers.TryGetValue(typeof(TEvent), out var handlers))
-            {
-                handlers.Add(activeHandler);
-            }
-            else
-                Handlers.Add(typeof(TEvent), new List<object> {activeHandler});
-        }
-
         public HandleResult<THandler, TEvent> HandleEvent<THandler, TEvent>(TEvent tEvent)
             where THandler : class, IEventHandler<TEvent>, new()
         {
-            if(Handlers.TryGetValue(tEvent.GetType(), out var handlers))
-            {
-                
-            }
-            
-        }
+            //TODO: Get MethodInfo-object from handlerinterface(IEventHandler)
+            //TODO: analyze if current handler is corresponding to current event, if no -> ignore
 
-        private Dictionary<Type, List<object>> Handlers { get; } = new();
+            var handlerInterface = AppDomain.CurrentDomain
+                .GetAssemblies().SelectMany(assembly =>
+                    assembly.GetTypes()).First(type =>
+                    type.IsInterface && type.IsGenericType && type.GenericTypeArguments.Contains(typeof(TEvent)));
+
+            var handlers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.IsClass && !type.IsAbstract && handlerInterface.IsAssignableTo(type));
+
+            foreach (var handler in handlers)
+            {
+                var method = handler.GetMethod(nameof(IEventHandler<TEvent>.Execute),
+                    new[] { typeof(IServiceProvider), typeof(TEvent), typeof(CancellationToken) });
+                
+                method.Invoke()
+            }
+        }
     }
 }
